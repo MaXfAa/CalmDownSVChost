dc603987114f6f913fac7c8d871b65e0fc5105db
Max <71935156+MaXfAa@users.noreply.github.com>
2022-01-26 18:59:03 +0100

    Add folder and files

---
+++ ShrinkSvcHost/→ 8 GB of memory ←/RUN-THIS.cmd
@@ -0,0 +1,10 @@
+@echo off
+cls
+echo Shrink the amount of svchost
+echo Validate the administrator execution request to pursue
+echo.
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0.\8.ps1""' -Verb RunAs}"
+echo Perfect!
+echo Now restart your computer to apply the changes
+echo.
+pause
