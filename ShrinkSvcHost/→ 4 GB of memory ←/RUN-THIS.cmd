39c1d65b6afc02cff2d84eecde656cb335805260
Max <71935156+MaXfAa@users.noreply.github.com>
2022-02-06 16:18:01 +0100

    edit "Validate" to "Confirm"

--- ShrinkSvcHost/→ 4 GB of memory ←/RUN-THIS.cmd
+++ ShrinkSvcHost/→ 4 GB of memory ←/RUN-THIS.cmd
@@ -1,7 +1,7 @@
 @echo off
 cls
 echo Shrink the amount of svchost
-echo Validate the administrator execution request to pursue
+echo Confirm the administrator execution request to pursue
 echo.
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0.\4.ps1""' -Verb RunAs}"
 echo Perfect!
